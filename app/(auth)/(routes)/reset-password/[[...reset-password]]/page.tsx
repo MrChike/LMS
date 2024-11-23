ee2daa0e7248d5b820b22d31bc112ca7960b36c8
chike <chikeegonu@gmail.com>
2024-11-23 17:00:05 +0100

    daily_commit: 2024-11-23 17:00:05

--- app/(auth)/(routes)/reset-password/[[...reset-password]]/page.tsx
+++ app/(auth)/(routes)/reset-password/[[...reset-password]]/page.tsx
@@ -1,6 +1,5 @@
 import ResetPassword from "./reset-password";
  
-// TODO: Create a Sign in Component in NextJs
 export default function Page() {
   return <ResetPassword />;
 }
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,5 @@
 import SignUp from "./sign-up"
  
-// TODO: Create a Sign out Component in NextJs
 export default function Page() {
   return <SignUp />;
 }
